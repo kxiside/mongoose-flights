b4fd039b94eddc88ec1895f1c06f9e47f77acb68
kxiside <swendside@gmail.com>
2023-08-22 11:58:46 -0700

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// connecting to url
+require('dotenv').config()
+// connecting to database
+require('./config/database')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
